172230f058484763bbe48bd4598781a3d1a0c96c
SUHAIL AKRAM MOHAMMED <suhailakram93@gmail.com>
2020-11-26 21:44:14 -0500

    Adding API for Delete

--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -69,6 +69,21 @@ namespace BulkyBook.Areas.Admin.Controllers
             var allObj = _unitOfWork.Category.GetAll();
             return Json(new { data = allObj });
         }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Deleted Successfully" });
+        }
+
+
         #endregion
 
     }
